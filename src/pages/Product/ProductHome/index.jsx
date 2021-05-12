c4781d7e5b34126f05ff68f9226ff623db93333e
495174699 <495174699@qq.com>
2021-05-12 19:50:12 +0800

    更新

--- src/pages/Product/ProductHome/index.jsx
+++ src/pages/Product/ProductHome/index.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
-
+import {Card,Form,Button,Input,Select} from 'antd'
+import { PlusOutlined } from '@ant-design/icons';
+const Option = Select.Option
 export default class ProductHome extends Component {
+
     render() {
+        const title = (
+            <span>
+                <Select style={{width:150}}>
+                    <Option>按名称搜索</Option>
+                    <Option>按描述搜索</Option>
+                </Select>
+                <Input placeholder="关键字"></Input>
+                {/* <Button>搜索</Button> */}
+            </span>
+        )
+        const extra = (
+            <div>
+                <Button><PlusOutlined />添加商品</Button>
+            </div>
+        )
         return (
             <div>
-                 ProductHome
+                <Card title={title} extra={extra} >
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
             </div>
         )
     }
